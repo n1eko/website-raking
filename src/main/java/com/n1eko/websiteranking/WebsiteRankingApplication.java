59eeb349bab492037ceed6af4d9fe8145965f8af
N1eko <aaroncabreira@gmail.com>
2023-08-19 22:38:36 +0200

    Initial commit

---
+++ src/main/java/com/n1eko/websiteranking/WebsiteRankingApplication.java
@@ -0,0 +1,13 @@
+package com.n1eko.websiteranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteRankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/n1eko/websiteranking/WebsiteRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n1eko.websiteranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
