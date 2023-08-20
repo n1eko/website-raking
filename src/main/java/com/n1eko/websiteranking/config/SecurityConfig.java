5ccadb2a511dc5691195f5e5dc919b22c077022e
N1eko <aaroncabreira@gmail.com>
2023-08-20 02:47:51 +0200

    Add CORS

---
+++ src/main/java/com/n1eko/websiteranking/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.n1eko.websiteranking.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${security.cors.allowed-origin}")
+    private String allowedOrigin;
+
+    /**
+     * Disable login, just for testing for the moment
+     **/
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList(allowedOrigin));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
