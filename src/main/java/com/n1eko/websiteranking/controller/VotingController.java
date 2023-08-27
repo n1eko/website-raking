c0d3c79dd4033b61cca86ea1d1b027bc1cc6ac8b
N1eko <aaroncabreira@gmail.com>
2023-08-27 17:36:42 +0200

    Debug headers.

--- src/main/java/com/n1eko/websiteranking/controller/VotingController.java
+++ src/main/java/com/n1eko/websiteranking/controller/VotingController.java
@@ -1,6 +1,5 @@
 package com.n1eko.websiteranking.controller;
 
-
 import com.n1eko.websiteranking.model.Vote;
 import com.n1eko.websiteranking.model.VoteType;
 import com.n1eko.websiteranking.model.Website;
@@ -8,6 +7,8 @@ import com.n1eko.websiteranking.service.VoteService;
 import com.n1eko.websiteranking.service.WebsiteService;
 import com.n1eko.websiteranking.utils.HttpUtils;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.Enumeration;
 import java.util.Optional;
 
 @CrossOrigin(origins = "${security.cors.allowed-origin}", maxAge = 3600)
@@ -22,6 +24,7 @@ import java.util.Optional;
 @RequestMapping("/api/v1")
 public class VotingController {
 
+    Logger logger = LoggerFactory.getLogger(VotingController.class);
     @Autowired
     private VoteService voteService;
     @Autowired
@@ -31,6 +34,7 @@ public class VotingController {
 
     @PostMapping("/vote")
     public ResponseEntity vote(@RequestParam Long websiteId, @RequestParam VoteType voteType, HttpServletRequest request) {
+        debugMethod(request);
         String clientIp = HttpUtils.getClientIpAddr(request);
         if (voteService.countVotesForIpWithinLast24Hours(clientIp) < maxAllowedVotesPerDay) {
             Optional<Website> website = websiteService.findWebsiteById(websiteId);
@@ -53,6 +57,16 @@ public class VotingController {
             }
         } else {
             return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
+    }
+
+    private void debugMethod(HttpServletRequest request){
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                logger.info("Header: " + request.getHeader(headerNames.nextElement()));
+            }
         }
     }
 
